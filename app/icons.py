e9711671cd647ee89f6ffa632d874509e3345ec6
Sander Huijsen <sander.huijsen@gmail.com>
2021-06-11 10:10:10 +0800

    Added files for colours and icons.

---
+++ app/icons.py
@@ -0,0 +1,16 @@
+import colours
+
+
+O = colours.BLACK
+X = colours.WHITE
+
+arrow = [
+    O, O, O, O, O, O, O, O,
+    O, O, O, X, O, O, O, O,
+    O, O, O, X, X, O, O, O,
+    X, X, X, X, X, X, O, O,
+    X, X, X, X, X, X, X, O,
+    X, X, X, X, X, X, O, O,
+    O, O, O, X, X, O, O, O,
+    O, O, O, X, O, O, O, O,
+]
